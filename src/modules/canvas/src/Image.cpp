7a0009ae6578b6b564dd86a6506b859ac60c9808
Samir Tine <samir.tine@luart.org>
2023-04-01 19:06:58 +0200

    Updated: Image:draw() method paremeter orders of ratio and opacity have been inverted Updated: Image() constructor won't be able to create empty Image anymore New: Image:drawrect() method

--- src/modules/canvas/src/Image.cpp
+++ src/modules/canvas/src/Image.cpp
@@ -9,9 +9,11 @@
 extern "C" {
     #include <luart.h>
     #include <File.h>
+    #include <Widget.h>
 }
 #include "d2d1.h"
 #include "Image.h"
+#include "Canvas.h"
 #include "Direct2D.h"
 
 luart_type TImage;
@@ -21,33 +23,23 @@ LUA_CONSTRUCTOR(Image) {
 	Image *img = (Image *)calloc(1, sizeof(Image));
     int type = lua_type(L, 3);
     img->d = (Direct2D *)lua_touserdata(L, 2);
-
-    if (type == LUA_TNUMBER) {
-        if (FAILED(img->d->Render->CreateBitmap(D2D1::SizeU(luaL_checknumber(L, 3), luaL_checknumber(L, 4)), D2D1::BitmapProperties(D2D1::PixelFormat(DXGI_FORMAT::DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE::D2D1_ALPHA_MODE_PREMULTIPLIED)), &img->Bitmap)))
-            luaL_error(L, "Failed to create Image object");
-    } else {
-        wchar_t *fname = luaL_checkFilename(L, 3);
-        IWICBitmapDecoder	  *IWICDecoder	 = nullptr;
-        IWICBitmapFrameDecode *IWICFrame	 = nullptr;
-        IWICFormatConverter	  *IWICConverter = nullptr;
-
-        if (SUCCEEDED(CoCreateInstance(CLSID_WICBmpDecoder, nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&IWICDecoder))))
-                if (SUCCEEDED(img->d->WICFactory->CreateDecoderFromFilename(fname, nullptr, GENERIC_READ, WICDecodeMetadataCacheOnDemand, &IWICDecoder)))
-                    if (SUCCEEDED(IWICDecoder->GetFrame(0, &IWICFrame)))
-                        if (SUCCEEDED(img->d->WICFactory->CreateFormatConverter(&IWICConverter)))
-                            if (SUCCEEDED(IWICConverter->Initialize(IWICFrame, GUID_WICPixelFormat32bppPBGRA, WICBitmapDitherTypeNone, nullptr, 0.0f, WICBitmapPaletteTypeCustom)));
-                                goto done;
-        luaL_error(L, "Failed to load Direct2D bitmap from file");
-    done:            
-        D2D1_BITMAP_PROPERTIES BitmapProperties = D2D1::BitmapProperties(D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_PREMULTIPLIED));
-
-        if (FAILED(img->d->Render->CreateBitmapFromWicBitmap(IWICConverter, nullptr, &img->Bitmap)))
-            luaL_error(L, "Failed to create Image object from Bitmap");
-
-        IWICDecoder->Release();
-        IWICFrame->Release();
-        IWICConverter->Release();
-    }
+    wchar_t *fname = luaL_checkFilename(L, 3);
+    IWICBitmapDecoder	  *IWICDecoder	 = nullptr;
+    IWICBitmapFrameDecode *IWICFrame	 = nullptr;
+    IWICFormatConverter	  *IWICConverter = nullptr;
+    D2D1_BITMAP_PROPERTIES BitmapProperties = D2D1::BitmapProperties(D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_PREMULTIPLIED));
+    
+    img->d->WICFactory->CreateDecoderFromFilename(fname, nullptr, GENERIC_READ, WICDecodeMetadataCacheOnLoad, &IWICDecoder);
+    if (!IWICDecoder)
+        luaL_error(L, "Failed to load Image from file");
+    IWICDecoder->GetFrame(0, &IWICFrame);
+    img->d->WICFactory->CreateFormatConverter(&IWICConverter);
+    IWICConverter->Initialize(IWICFrame, GUID_WICPixelFormat32bppPBGRA, WICBitmapDitherTypeNone, nullptr, 0.0f, WICBitmapPaletteTypeCustom);      
+    if (FAILED(img->d->Render->CreateBitmapFromWicBitmap(IWICConverter, nullptr, &img->Bitmap)))
+        luaL_error(L, "Failed to create Image object from Bitmap");
+    IWICDecoder->Release();
+    IWICFrame->Release();
+    IWICConverter->Release();
 	lua_newinstance(L, img, Image);
 	return 1;
 }
@@ -57,9 +49,16 @@ LUA_METHOD(Image, draw) {
     const D2D1_SIZE_U size = img->Bitmap->GetPixelSize();
     float x = static_cast<FLOAT>(luaL_checknumber(L, 2));
     float y = static_cast<FLOAT>(luaL_checknumber(L, 3));
-    float ratio = static_cast<FLOAT>(luaL_optnumber(L, 5, 1.0f));
-    
-    img->d->Render->DrawBitmap(img->Bitmap, D2D1::RectF(x, y, x+size.width*ratio, y+size.height*ratio), luaL_optnumber(L, 4, 1.0f), D2D1_BITMAP_INTERPOLATION_MODE_LINEAR, NULL);
+    float ratio = static_cast<FLOAT>(luaL_optnumber(L, 4, 1.0f));
+
+    img->d->Render->DrawBitmap(img->Bitmap, D2D1::RectF(x, y, x+size.width*ratio, y+size.height*ratio), luaL_optnumber(L, 5, 1.0f), D2D1_BITMAP_INTERPOLATION_MODE_LINEAR, NULL);
+    return 0;
+}
+
+LUA_METHOD(Image, drawrect) {
+    Image *img = lua_self(L, 1, Image);
+   
+    img->d->Render->DrawBitmap(img->Bitmap, D2D1::RectF(static_cast<FLOAT>(luaL_checknumber(L, 2)), static_cast<FLOAT>(luaL_checknumber(L, 3)), static_cast<FLOAT>(luaL_checknumber(L, 4)), static_cast<FLOAT>(luaL_checknumber(L, 5))), luaL_optnumber(L, 6, 1.0f), D2D1_BITMAP_INTERPOLATION_MODE_LINEAR, NULL);
     return 0;
 }
 
@@ -77,6 +76,7 @@ OBJECT_MEMBERS(Image)
     READONLY_PROPERTY(Image, width)
     READONLY_PROPERTY(Image, height)
     METHOD(Image, draw)
+    METHOD(Image, drawrect)
 END
 
 LUA_METHOD(Image, __gc) {
