7eaa9e85407341678efeaa41889d341eb5bcc86c
Samir Tine <samir.tine@luart.org>
2023-11-18 14:19:26 +0100

    New: `json.load()` and `json.save()` to load/save lua table from/to a JSON file Fixed : `json.encode()` may crash due to failed memory allocation #117 Fixed : `json.encode()` cannot encode Lua `nil` values to JSON `null` #118

--- src/modules/json/src/json.c
+++ src/modules/json/src/json.c
@@ -1,5 +1,6 @@
 #include "zzzJSON.h"
 #include <luart.h>
+#include <File.h>
 
 static Allocator *A;
 
@@ -40,7 +41,10 @@ static void LuaToJson(lua_State *L, Value *v) {
             }
         } break;
 
-        default: SetStrFast(v, lua_tostring(L, -1));
+        default: if (strcmp(lua_tostring(L, -1), "null") == 0)
+                    SetNull(v);
+                 else
+                    SetStrFast(v, lua_tostring(L, -1));
     }
 }
 
@@ -81,7 +85,7 @@ static void JsonToLua(lua_State *L, Value *v) {
             break;
 
         case JSONTypeNull:
-            lua_pushnil(L);
+            lua_pushstring(L, "null");
             break;
 
         case JSONTypeString:
@@ -111,10 +115,32 @@ LUA_METHOD(json, decode)
     if (!ParseFast(src_v, src))
         return 0;
     JsonToLua(L, src_v);
-    ReleaseAllocator(A);
     return 1;
 }
 
+//------------------------------------ json.load() function
+LUA_METHOD(json, load)
+{
+    Value *src_v = NewValue(A);
+    wchar_t *fname = luaL_checkFilename(L, 1);
+    FILE *f = _wfopen(fname, L"rb");
+    int result = 0;
+    
+    if (f) {
+      fseek(f, 0, SEEK_END);
+      long long fsize = _ftelli64(f);
+      fseek(f, 0, SEEK_SET);
+      char *src = calloc(1, fsize + 1);
+      if(fread(src, fsize, 1, f) && ParseFast(src_v, src)) {
+        JsonToLua(L, src_v);
+        result = 1;
+      }
+      free(src);
+    }
+    free(fname);
+    fclose(f);
+    return result;
+}
 //------------------------------------ json.encode() function
 LUA_METHOD(json, encode)
 {
@@ -123,6 +149,25 @@ LUA_METHOD(json, encode)
     lua_pushvalue(L, 1);
     LuaToJson(L, v);
     lua_pushstring(L, Stringify(v));
+    return 1;
+}
+
+//------------------------------------ json.save() function
+LUA_METHOD(json, save)
+{
+    wchar_t *fname = luaL_checkFilename(L, 1);
+    FILE *f = _wfopen(fname, L"wb");
+    
+    if (f) {
+        Value *v = NewValue(A);
+        luaL_checktype(L, 2, LUA_TTABLE);
+        lua_pushvalue(L, 2);
+        LuaToJson(L, v);
+        const char *str = Stringify(v);
+        lua_pushboolean(L, fwrite(str, strlen(str), 1, f));
+        fclose(f);      
+    }
+    free(fname);
     return 1;
 }
 
@@ -136,12 +181,13 @@ static const luaL_Reg json_properties[] = {
   {NULL, NULL}
 };
 
-//--- luaL_Reg array for module functions, postfixed by "lib"  
-static const luaL_Reg jsonlib[] = {
-  {"decode",		json_decode}, 
-  {"encode",		json_encode}, 
-  {NULL, NULL}
-};
+//--- Register module functions, postfixed by "lib"  
+MODULE_FUNCTIONS(json)
+  METHOD(json, decode)
+  METHOD(json, encode)
+  METHOD(json, load)
+  METHOD(json, save)
+END
 
 //----- "calc" module registration function
 int __declspec(dllexport) luaopen_json(lua_State *L)
