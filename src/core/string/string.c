25b774db963f20fbce5ae984a309f4a38e587824
samyeyo <samir.tine@luart.org>
2024-10-28 14:24:07 +0100

    New: `string.normalize()` function to replace accentued characters by non accentued ones in a string

--- src/core/string/string.c
+++ src/core/string/string.c
@@ -2008,6 +2008,18 @@ int str_split(lua_State *L) {
     return 1;  
 }
 
+int str_normalize(lua_State *L) {
+	char *buff;
+  wchar_t *str = lua_towstring(L, 1);
+	int size = WideCharToMultiByte(20127, 0, str, -1, NULL, 0, NULL, NULL);
+	buff = (char *)malloc(size);
+	WideCharToMultiByte(20127, 0, str, -1, buff, size, NULL, NULL);
+  lua_pushstring(L, buff);
+	free(buff);
+  free(str);
+  return 1;
+}
+
 /* }====================================================== */
 
 extern const luaL_Reg strlib[];
@@ -2033,6 +2045,7 @@ static const luaL_Reg wstrlib[] = {
   {"unpack", str_unpack},
   {"usearch", str_search},
   {"split", str_split},
+  {"normalize", str_normalize},
   {NULL, NULL}
 };
 
