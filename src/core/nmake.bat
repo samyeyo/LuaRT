75fd62c93753f479ee854e804b1c7e2a950a04ad
samyeyo <samir.tine@luart.org>
2024-10-28 14:16:08 +0100

    Updated: makefiles

--- src/core/nmake.bat
+++ src/core/nmake.bat
@@ -1,8 +1,7 @@
 @echo off
-@setlocal EnableDelayedExpansion
+@chcp 65001 >nul 2>&1
 
-if exist ..\..\..\bin\luart.exe (
-    for /f %%i in ('..\..\..\bin\luart.exe -e "print(_ARCH)"') do nmake.exe /nologo PLATFORM=%%i %1
-) else (
-    nmake.exe /nologo %1
-)
+FOR /F "delims=" %%i IN ('where luart.exe 2^>nul') DO set p=%%~di%%~pi..
+IF not DEFINED p SET p=..\..\..
+
+nmake.exe /nologo %1 LUART_PATH=%p% PLATFORM=%SDK_ARCH%
--- src/nmake.bat
+++ src/nmake.bat
@@ -1,3 +1,3 @@
 @echo off
 set dir=%cd%
-for /f %%i in ('dir /ad/b "%dir%"') do (@if exist %%i/nmake.bat (cd %%i & nmake.bat %1 & cd ..) else (@if exist %%i/Makefile (cd %%i & nmake.exe %1 & cd ..)))
+for /f %%i in ('dir /ad/b "%dir%"') do @(@if exist %%i/nmake.bat @(@cd %%i & nmake.bat %1 & @cd ..) else @(@if exist %%i/Makefile @(@cd %%i & nmake.exe %1 & @cd ..)))
