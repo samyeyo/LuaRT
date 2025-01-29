18eb0b3c1ed9579d9318392182896d0aef075312
samyeyo <samir.tine@luart.org>
2025-01-29 18:22:47 +0100

    Updated copyright year to 2025

--- include/Pipe.h
+++ include/Pipe.h
@@ -1,6 +1,6 @@
 /*
  | LuaRT - A Windows programming framework for Lua
- | Luart.org, Copyright (c) Tine Samir 2024
+ | Luart.org, Copyright (c) Tine Samir 2025
  | See Copyright Notice in LICENSE.TXT
  |-------------------------------------------------
  | Pipe.h | LuaRT Pipe object header
--- include/Widget.h
+++ include/Widget.h
@@ -1,22 +1,23 @@
 /*
  | LuaRT - A Windows programming framework for Lua
- | Luart.org, Copyright (c) Tine Samir 2024
+ | Luart.org, Copyright (c) Tine Samir 2025
  | See Copyright Notice in LICENSE.TXT
  |-------------------------------------------------
  | Widget.h | LuaRT Widget object header
 */
-
-#define LUA_LIB
-
-#include <luart.h>
-#include <commctrl.h>
-#include <windows.h>
 
 #pragma once
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#include <luart.h>
+#define LUA_LIB
+
+
+#include <windows.h>
+#include <commctrl.h>
 
 extern int UIWindow;
 extern int UIButton;
@@ -83,6 +84,7 @@ typedef struct Widget {
 	void		*user;
 	BOOL		ismain;
 	BOOL		isactive;
+	BOOL 		allowdrop;
 } Widget;
 
 //--- Register a widget type
@@ -91,10 +93,71 @@ typedef struct Widget {
 #define lua_regparentwidget(L, __typename, methods, mt, has_text, has_font, has_cursor, has_icon, has_tooltip, TRUE) lua_registerwidget(L, &T##__typename, #__typename, __typename##_constructor, methods, mt, has_text, has_font, has_cursor, has_icon, FALSE, FALSE, has_tooltip, TRUE, FALSE)
 #define lua_regparentwidgetmt(L, __typename, methods, has_text, has_font, has_cursor, has_icon, has_tooltip, TRUE) lua_registerwidget(L, &T##__typename, #__typename, __typename##_constructor, methods, __typename##_metafields, has_text, has_font, has_cursor, has_icon, FALSE, FALSE, has_tooltip, TRUE, FALSE)
 
-LUA_API lua_Integer lua_registerevent(lua_State *L, const char *methodname, lua_CFunction event) ;
+typedef int (*lua_Event)(lua_State *L, Widget *w, MSG *msg);
+
+LUA_API lua_Integer lua_registerevent(lua_State *L, const char *methodname, lua_Event event) ;
 LUA_API void *lua_getevent(lua_State *L, lua_Integer eventid, int *type);
 
 #define WM_LUAEVENTRESULT 0
+
+#define	WM_LUAMIN			(WM_USER+2)
+#define WM_LUAHIDE 			(WM_LUAMIN)
+#define WM_LUASHOW 			(WM_LUAMIN + 1)
+#define WM_LUAMOVE 			(WM_LUAMIN + 2)
+#define WM_LUARESIZE 		(WM_LUAMIN + 3)
+#define WM_LUAHOVER 		(WM_LUAMIN + 4)
+#define WM_LUALEAVE 		(WM_LUAMIN + 5)
+#define WM_LUACLOSE 		(WM_LUAMIN + 6)
+#define WM_LUACLICK 		(WM_LUAMIN + 7)
+#define WM_LUADBLCLICK		(WM_LUAMIN + 8)
+#define WM_LUACONTEXT 		(WM_LUAMIN + 9)
+#define WM_LUACREATE	 	(WM_LUAMIN + 10)
+#define WM_LUACARET 		(WM_LUAMIN + 11)
+#define WM_LUACHANGE 		(WM_LUAMIN + 12)
+#define WM_LUASELECT 		(WM_LUAMIN + 13)
+#define WM_LUATRAYCLICK 	(WM_LUAMIN + 14)
+#define WM_LUATRAYDBLCLICK	(WM_LUAMIN + 15)
+#define WM_LUATRAYCONTEXT 	(WM_LUAMIN + 16)
+#define WM_LUATRAYHOVER 	(WM_LUAMIN + 17)
+#define WM_LUANOTIFYCLICK 	(WM_LUAMIN + 18)
+#define WM_LUAMENU 			(WM_LUAMIN + 19)
+#define WM_LUAKEY 			(WM_LUAMIN + 20)
+#define WM_LUAMOUSEUP 		(WM_LUAMIN + 21)
+#define WM_LUAMOUSEDOWN 	(WM_LUAMIN + 22)
+#define WM_LUAMAXIMIZE  	(WM_LUAMIN + 23)
+#define WM_LUAMINIMIZE   	(WM_LUAMIN + 24)
+#define WM_LUARESTORE    	(WM_LUAMIN + 25)
+#define WM_LUADROP    	    (WM_LUAMIN + 26)
+
+typedef enum {
+	onHide			= WM_LUAHIDE,
+    onShow 			= WM_LUASHOW, 
+    onMove 			= WM_LUAMOVE, 
+    onResize		= WM_LUARESIZE,
+    onHover 		= WM_LUAHOVER,
+    onLeave			= WM_LUALEAVE,
+    onClose 		= WM_LUACLOSE,
+    onClick 		= WM_LUACLICK,
+    onDoubleClick 	= WM_LUADBLCLICK,
+    onContext 		= WM_LUACONTEXT,
+    onCreate		= WM_LUACREATE,
+    onCaret 		= WM_LUACARET,
+    onChange 		= WM_LUACHANGE,
+    onSelect 		= WM_LUASELECT,
+    onTrayClick		= WM_LUATRAYCLICK,
+    onTrayDoubleClick=WM_LUATRAYDBLCLICK,
+    onTrayContext 	= WM_LUATRAYCONTEXT,
+    onTrayHover 	= WM_LUATRAYHOVER,
+    onNotificationClick	= WM_LUANOTIFYCLICK,
+	onMenu			= WM_LUAMENU,
+	onKey			= WM_LUAKEY,
+    onMouseUp       = WM_LUAMOUSEUP,
+    onMouseDown     = WM_LUAMOUSEDOWN,
+    onMaximize      = WM_LUAMAXIMIZE,
+    onMinimize      = WM_LUAMINIMIZE,
+    onRestore       = WM_LUARESTORE,
+    onDrop          = WM_LUADROP,
+} WidgetEvent;
 
 //---- call event e associated with widget w
 #define lua_callevent(w, e) PostMessage((HWND)w->handle, e, 0, 0)
