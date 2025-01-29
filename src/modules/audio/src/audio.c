88dfb46a8c4f80b1422af29ef8ad14a4b81864d7
samyeyo <samir.tine@luart.org>
2025-01-29 15:56:37 +0100

    - New: `audio.playdevice` and `audio.recdevice` properties to set/get the current playback and record devices - New: `audio.devices` property to enumerate audio devices - New: `audio.record.start()` and `audio.record.stop()` methods to record and encode audio - Updated: `Sound.play()` now returns a `Task` that will complete once the sound has finished playing. - Removed: `audio.device` property have been removed.

--- src/modules/audio/src/audio.c
+++ src/modules/audio/src/audio.c
@@ -1,16 +1,14 @@
 /*
  | LuaRT - A Windows programming framework for Lua
- | Luart.org, Copyright (c) Tine Samir 2024
+ | Luart.org, Copyright (c) Tine Samir 2025
  | See Copyright Notice in LICENSE.TXT
  |-------------------------------------------------
  | audio.c | LuaRT audio module
 */
 
 #include <luart.h>
-#include <File.h>
-
-#define STB_VORBIS_HEADER_ONLY
-#include "stb_vorbis.c"   
+#include <File.h> 
+#include "encoder.h"
 
 #define MINIAUDIO_IMPLEMENTATION
 #include "miniaudio.h"
@@ -26,11 +24,19 @@
 const char *vector_members[] = { "x", "y", "z" };
 const char *cone_members[] = { "inangle", "outangle", "outgain" };
 
-ma_engine *engine;
+typedef struct {
+  ma_device_id    *id;
+  char            name[256];
+  Encoder         *encoder;
+} ma_record;
+
+ma_engine *engine = NULL;
+ma_record record = {0};
+
 static float volume = 1, gain = 0;
 
 int set_vector(lua_State *L, ma_vec3f *v, const char **members) {
-  lua_createtable(L, 3, 0);
+  lua_createtable(L, 3, 0); 
   for (int i = 0; i < 3; i++) {
     lua_pushstring(L, members[i]);
     lua_pushnumber(L, *(float*)(((char*)v)+(i*sizeof(float))));
@@ -96,9 +102,98 @@ LUA_PROPERTY_GET(audio, samplerate) {
   return 1;
 }
 
-LUA_PROPERTY_GET(audio, device) {
+LUA_PROPERTY_GET(audio, playdevice) {
   ma_device *dev = ma_engine_get_device(engine);
   lua_pushstring(L, dev->playback.name);
+  return 1;
+}
+
+LUA_PROPERTY_GET(audio, recdevice) {
+  if (record.name[0] == 0) {
+    ma_device_config deviceConfig;
+    ma_device device;
+    deviceConfig = ma_device_config_init(ma_device_type_capture);
+    deviceConfig.capture.format = ma_format_f32;
+    deviceConfig.capture.channels = 2;
+    deviceConfig.sampleRate = 44100;
+    deviceConfig.dataCallback = NULL;
+    deviceConfig.pUserData = NULL;
+    if (ma_device_init(NULL, &deviceConfig, &device) == MA_SUCCESS) {
+      strncpy(record.name, device.capture.name, 256);
+      ma_device_uninit(&device);
+    } else strncpy(record.name, "unknown", 7);
+  }
+  lua_pushstring(L, record.name); 
+  return 1;
+}
+
+static BOOL enum_devices(ma_device_info **pPlaybackDeviceInfos, ma_uint32 *pcount, ma_device_info ** pRecDeviceInfos, ma_uint32 *rcount) {
+  ma_context context; 
+  BOOL result = !ma_context_init(NULL, 0, NULL, &context) && !ma_context_get_devices(&context, pPlaybackDeviceInfos, pcount, pRecDeviceInfos, rcount);
+  ma_context_uninit(&context); 
+  return result;
+}
+
+LUA_PROPERTY_SET(audio, playdevice) {
+  const char *device_name = luaL_checkstring(L, 1);
+  ma_device_info *devices;
+  ma_uint32 count; 
+
+  if (enum_devices(&devices, &count, NULL, NULL)) {
+    for (int i = 0; i < count; i++) {
+      if (strcmp(devices[i].name, device_name) == 0) {
+        ma_engine_config engineConfig = ma_engine_config_init();
+        ma_engine_uninit(engine);
+        engineConfig.pPlaybackDeviceID = &(devices[i].id);
+        engineConfig.noDevice = MA_FALSE;
+
+        if (ma_engine_init(&engineConfig, engine) || ma_engine_start(engine)) {
+          ma_engine_uninit(engine);
+          ma_engine_init(NULL, engine);
+        } else break;
+      }
+    }
+  }
+  return 0;
+}
+
+LUA_PROPERTY_SET(audio, recdevice) {
+  const char *device_name = luaL_checkstring(L, 1);
+  ma_device_info *devices;
+  ma_uint32 count; 
+
+  if (enum_devices(NULL, NULL, &devices, &count)) {
+    for (int i = 0; i < count; i++) {
+      if (strcmp(devices[i].name, device_name) == 0) {
+        record_stop(L);
+        record.id = &devices[i].id;
+        strncpy(record.name, devices[i].name, 256);
+      }
+    }
+  }
+  return 0;
+}
+
+LUA_PROPERTY_GET(audio, devices) {
+  ma_device_info *devices, *recdevices;
+  ma_uint32 pcount, rcount; 
+  int i; 
+
+  lua_createtable(L, 0, 2);
+  if (enum_devices(&devices, &pcount, &recdevices, &rcount)) {
+    lua_createtable(L, pcount, 0);
+    for (i = 0; i < pcount; i++) {
+      lua_pushstring(L, devices[i].name);
+      lua_rawseti(L, -2, i+1);
+    }
+    lua_setfield(L, -2, "playback");
+    lua_createtable(L, rcount, 0);
+    for (i = 0; i < rcount; i++) {
+      lua_pushstring(L, recdevices[i].name);
+      lua_rawseti(L, -2, i+1);
+    }
+    lua_setfield(L, -2, "recording");
+  } 
   return 1;
 }
 
@@ -111,12 +206,39 @@ LUA_PROPERTY_SET(audio, gain) {
 LUA_METHOD(audio, play) {
   wchar_t *fname = luaL_checkFilename(L, 1);
   ma_result r = ma_engine_play_sound(engine, fname, NULL);
- 
+  
   free(fname);
   if (r != MA_SUCCESS)
     luaL_error(L, ma_result_description(r));
   lua_pushboolean(L, r == MA_SUCCESS);
   return 1;
+}
+
+typedef void(*EncoderCallback)(ma_device*, void*, const void*, ma_uint32);
+
+LUA_METHOD(record, stop) {
+    if (record.encoder) {
+      Encoder_Release(record.encoder);
+      memset(&record, 0, sizeof(record));
+  }
+  return 0;
+}
+
+LUA_METHOD(record, start) {
+  record_stop(L);
+  wchar_t *fname = luaL_checkFilename(L, 1);
+  ma_device_config deviceConfig;
+  EncoderType format = lua_optstring(L, 2, AudioFormatNames, 0);
+  int channels = luaL_optinteger(L, 3, 2);
+  int sampleRate = luaL_optinteger(L, 4, 44100);
+  int bitrate = luaL_optinteger(L, 5, 0);
+
+  if (format == -1)
+    luaL_error(L, "'%s' encoder not supported", lua_tostring(L, 2));
+  if (!(record.encoder = Encoder_Create(fname, format, channels, sampleRate, bitrate, &record.id)))
+    luaL_error(L, "failed to initialize %s encoder", AudioFormatNames[format]);
+  free(fname);
+  return 0;
 }
 
 //------------------------------- audio module properties definition
@@ -131,7 +253,9 @@ MODULE_PROPERTIES (audio)
   READWRITE_PROPERTY(audio, gain)
   READONLY_PROPERTY(audio, channels)
   READONLY_PROPERTY(audio, samplerate)
-  READONLY_PROPERTY(audio, device)
+  READWRITE_PROPERTY(audio, playdevice)
+  READWRITE_PROPERTY(audio, recdevice)
+  READONLY_PROPERTY(audio, devices)
 END
 
 //------------------------------- audio module functions definition
@@ -140,15 +264,22 @@ MODULE_FUNCTIONS(audio)
   METHOD(audio, play)
 END
 
+MODULE_FUNCTIONS(record)
+  METHOD(record, start)
+  METHOD(record, stop)
+END
+
 //------------------------------- audio module finalizer
 
 int audio_finalize(lua_State *L)
 { 
+  record_stop(L);
   if (engine) {
     ma_engine_uninit(engine);
     free(engine); 
     engine = NULL;
   }
+  MFShutdown();
   return 0;
 }
 
@@ -214,13 +345,16 @@ BOOL WINAPI DllMain( IN HINSTANCE hDllHandle, DWORD nReason, LPVOID Reserved ) {
 int __declspec(dllexport) luaopen_audio(lua_State *L)
 {
   engine = calloc(1, sizeof(ma_engine));
-  ma_result r = ma_engine_init(NULL, engine);
-
-  if (r != MA_SUCCESS) {
+  if (ma_engine_init(NULL, engine) != MA_SUCCESS) {
     free(engine);
-    luaL_error(L, ma_result_description(r));
+    luaL_error(L, "fatal error initalizing audio module");
   }
+  MFStartup(MF_VERSION, MFSTARTUP_NOSOCKET);
   lua_regmodulefinalize(L, audio);
+  lua_pushstring(L, "record");
+  lua_createtable(L, 0, 2);
+  luaL_setfuncs(L, recordlib, 0);
+  lua_rawset(L, -3);
   lua_regobjectmt(L, Sound);
   return 1;
 }
